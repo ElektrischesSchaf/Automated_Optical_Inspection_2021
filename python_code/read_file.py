7bc9fa9bd1e1136d6a3e7592627a9b9a6b314758
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2021-03-11 16:45:47 +0000

    AD

--- python_code/read_file.py
+++ python_code/read_file.py
@@ -1,5 +1,88 @@
 import matplotlib.pyplot as plt
 import torchvision
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch
+from PIL import Image
+
+import os
+
+import pandas as pd
+
+batch_size=64
+
+# transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform=transforms.ToTensor()
+
+def default_loader(path):
+    return torchvision.dataset.ImageFolder(
+        root =  path,
+        transform = torchvision.transforms.ToTensor()
+    )
+
+# https://stackoverflow.com/questions/50052295/how-do-you-load-mnist-images-into-pytorch-dataloader
+class MyTrainDataset(Dataset):
+    def __init__(self, text_file,root_dir, transform=transform):
+        self.csv_file=pd.read_csv(text_file)
+        self.name_frame=self.csv_file['ID']
+        self.label_frame=self.csv_file['Label']
+        self.root_dir = root_dir
+        self.transform = transform
+    
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, str(self.name_frame.iloc[idx] ))
+        labels = self.label_frame.iloc[idx]
+        image = Image.open(img_name)
+        image = self.transform(image)
+        sample={'image':image, 'labels':labels}
+        return sample
+
+    def __len__(self):
+        return len(self.name_frame)
+
+class MyTestDataset(Dataset):
+    def __init__(self, text_file,root_dir, transform=transform):
+        self.csv_file=pd.read_csv(text_file)
+        self.name_frame=self.csv_file['ID']
+
+        self.root_dir = root_dir
+        self.transform = transform
+    
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, str(self.name_frame.iloc[idx] ))
+        image = Image.open(img_name)
+        image = self.transform(image)
+        sample={'image':image}
+        return sample
+
+    def __len__(self):
+        return len(self.name_frame)
+
+
+train_dataset = MyTrainDataset( text_file= './dataset/train.csv' , root_dir='./dataset/train_images/train_images/')
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+
+test_dataset = MyTestDataset( text_file= './dataset/test.csv' , root_dir='./dataset/test_images/test_images/')
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+print(type(train_dataset))
+print(type(test_dataset))
+
+for batch_idx, data in enumerate(train_dataloader):
+    print (data['labels'])
+    # print('\n')
+    # print (data['image'])
+
+
+for batch_idx, data in enumerate(test_dataloader):
+	print (data['image'].size())
+
+'''
+import matplotlib.pyplot as plt
+import torchvision
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
@@ -48,3 +131,4 @@ for batch_idx, (data, target) in enumerate(load_train_dataset()):
 
 for batch_idx, (data, target) in enumerate(load_test_dataset()):
 	print (batch_idx)
+'''
